f65059940657be1b476168a9f7d178b3c1c16e94
JonashGana <jonashtiglao@gmail.com>
2025-01-08 22:42:00 +0800

    changed import locations

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthProvider'
 import './style/index.css'
-import App from './components/App'
+import App from './App'
 
 
 createRoot(document.getElementById('root')!).render(
