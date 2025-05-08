d64dc27e2f07f40e550d13fdfc419890846cd157
JonashGana <jonashtiglao@gmail.com>
2025-05-08 22:49:59 +0800

    small change

--- src/components/Sections/Playlists.tsx
+++ src/components/Sections/Playlists.tsx
@@ -1,59 +1,69 @@
-import { useAuth } from "@/context/AuthProvider"
-import { useEffect, useState } from "react"
-import { getUserPlaylists } from "@/api"
-import { UserPlaylists } from "@/types"
-import { Swiper, SwiperSlide } from "swiper/react" 
-import { FreeMode, Navigation } from "swiper/modules"
+import { useAuth } from "@/context/AuthProvider";
+import { useEffect, useState } from "react";
+import { getUserPlaylists, getUserPlaylistSongs } from "@/api";
+import { UserPlaylists } from "@/types";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Playlists = () => {
-    const { accessToken }  = useAuth()
-    const [playlists, setPlaylists] = useState<UserPlaylists[]>();
+  const { accessToken } = useAuth();
+  const [playlists, setPlaylists] = useState<UserPlaylists[]>();
 
-    useEffect(() => {
-        if (accessToken) {
-            getUserPlaylists(accessToken).then(setPlaylists);
-        }
-    }, [accessToken])
-
-    if (!playlists) {
-        return <h1>loading...</h1>
+  useEffect(() => {
+    if (accessToken) {
+      getUserPlaylists(accessToken).then(setPlaylists);
     }
+  }, [accessToken]);
 
-    return (
-        <div className="max-w-[1000px] w-full mx-auto pt-16 pb-24">
-            <h1 className="text-white font-bold text-3xl mb-6">Playlists</h1>
-            <Swiper
-                slidesPerView={'auto'}
-                navigation={true}
-                freeMode={true}
-                centeredSlides={false}
-                modules={[ Navigation, FreeMode ]}
-                className="mySwiper w-full playlist-swiper"
-            >
-                {playlists.map((playlist) => (
-                    <SwiperSlide key={playlist.id} className="max-w-[150px] mr-6">
-                        <div className="w-full flex flex-col">
-                            <div className="w-full">
-                                <img src={playlist.images[0].url} alt="playlist cover" className="w-full h-[150px] object-cover rounded-md"/>
-                            </div>
-                            <div className="pt-2 flex flex-col">
-                                <span className="text-white">{playlist.name}</span>
-                                {playlist.tracks.total === 0 ? (
-                                    <span className="text-zinc-300 text-sm">By {playlist.owner.display_name}</span>
-                                ) : (
-                                    <span className="text-zinc-300 text-sm">{playlist.tracks.total} songs</span>
-                                )}
-                                <p className="text-zinc-400 pt-2 leading-4">{playlist.description}</p>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    )
-}
+  if (!playlists) {
+    return <h1>loading...</h1>;
+  }
 
-export default Playlists
+  return (
+    <div className="max-w-[1000px] w-full mx-auto pt-16 pb-24">
+      <h1 className="text-white font-bold text-3xl mb-6">Playlists</h1>
+      <Swiper
+        slidesPerView={"auto"}
+        navigation={true}
+        freeMode={true}
+        centeredSlides={false}
+        modules={[Navigation, FreeMode]}
+        className="mySwiper w-full playlist-swiper"
+      >
+        {playlists.map((playlist) => (
+          <SwiperSlide key={playlist.id} className="max-w-[150px] mr-6">
+            <div className="w-full flex flex-col">
+              <div className="w-full">
+                <img
+                  src={playlist.images[0].url}
+                  alt="playlist cover"
+                  className="w-full h-[150px] object-cover rounded-md"
+                />
+              </div>
+              <div className="pt-2 flex flex-col">
+                <span className="text-white">{playlist.name}</span>
+                {playlist.tracks.total === 0 ? (
+                  <span className="text-zinc-300 text-sm">
+                    By {playlist.owner.display_name}
+                  </span>
+                ) : (
+                  <span className="text-zinc-300 text-sm">
+                    {playlist.tracks.total} songs
+                  </span>
+                )}
+                <p className="text-zinc-400 pt-2 leading-4">
+                  {playlist.description}
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Playlists;
