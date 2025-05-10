308e95740faac6d0eb12b3995c36dfe6292ed739
JonashGana <jonashtiglao@gmail.com>
2025-05-10 23:54:12 +0800

    added api fetch functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -149,6 +149,26 @@ export const getUserPlaylists = async (accessToken: string) => {
   }
 };
 
+export const getPlaylistMetadata = async (
+  playlistId: string,
+  accessToken: string
+) => {
+  try {
+    const response = await axios.get(
+      `https://api.spotify.com/v1/playlists/${playlistId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching playlist songs", error);
+  }
+};
+
 export const getUserPlaylistSongs = async (
   offset: number,
   limit: number,
