42f402d3de84d680abf83eb84c1295eddcef8aa4
JonashGana <jonashtiglao@gmail.com>
2025-05-10 23:53:54 +0800

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import { useAuth } from "./context/AuthProvider"
-import AuthLayout from "./layout/AuthLayout";
+import { useAuth } from "./context/AuthProvider";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
+import PlaylistPage from "./pages/PlaylistPage";
 
 const App = () => {
-    const { login, isAuthenticated } = useAuth();
-    return (
-        <div className="w-full min-h-screen bg-dark-color">
-            { isAuthenticated ? (
-                <MainLayout />
-            ) : (
-                <AuthLayout handleSignIn={login}/>
-            )}
-        </div>
-    )
-}
+  const { login, isAuthenticated } = useAuth();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? (
+                <HomePage />
+              ) : (
+                <LoginPage handleSignIn={login} />
+              )
+            }
+          />
+          <Route
+            path="/playlists/:playlistId"
+            element={isAuthenticated ? <PlaylistPage /> : <Navigate to="/" />}
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
