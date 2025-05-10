c64212a592abd24f0eaf264830c018f5a3d3aa37
JonashGana <jonashtiglao@gmail.com>
2025-05-10 23:55:11 +0800

    modified main layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,19 +1,11 @@
-import Hero from "@/components/Sections/Hero"
-import WrappedSwiper from "@/components/Sections/WrappedSwiper"
-import Masonry from "@/components/Sections/Masonry"
-import Playlists from "@/components/Sections/Playlists"
-import Footer from "@/components/Sections/Footer"
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
-    return (
-        <div className="w-full px-4 sm:px-8 pt-8">
-            <Hero />
-            <WrappedSwiper />
-            <Masonry />
-            <Playlists />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="w-full min-h-screen bg-dark-color">
+      <Outlet />
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
